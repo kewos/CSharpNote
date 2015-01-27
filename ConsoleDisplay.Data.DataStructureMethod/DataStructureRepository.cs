7f769a22ed1081b883d7e7559fd116b1d369b95b
kewos <gn00437805@gmail.com>
2015-01-27 17:05:52 +0800

    update

---
+++ ConsoleDisplay.Data.DataStructureMethod/DataStructureRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleDisplay.Common;
+using ConsoleDisplay.Core.Contracts;
+using ConsoleDisplay.Core.Implements;
+
+namespace ConsoleDisplay.Data.DataStructureMethod
+{
+    public class DataStructureMethodRepository : AbstractMethodRepository
+    {
+
+    }
+}
