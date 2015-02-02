5c9c8fb496ef27ff95633e2f66c5ecdb7204e0b8
kewos <gn00437805@gmail.com>
2015-02-02 17:03:28 +0800

    update

--- ConsoleDisplay.Data.DataStructureMethod/DataStructureRepository.cs
+++ ConsoleDisplay.Data.DataStructureMethod/DataStructureRepository.cs
@@ -28,21 +28,66 @@ namespace ConsoleDisplay.Data.DataStructureMethod
         }
 
         [DisplayMethod]
+        public void CircularQueueImplement()
+        {
+            var circularQueue = new CircularQueue<int>(3).Enqueue(1).Enqueue(2).Enqueue(3);
+            var assert = new List<int> { 1, 2, 3 };
+            circularQueue.All((index, element) => element == assert[index]).ToConsole("elements is {1, 2, 3}:");
+            (circularQueue.Count == 3).ToConsole("Count is 3:");
+
+            "\n".ToConsole();
+            var circularQueue1 = new CircularQueue<int>(3).Enqueue(1).Enqueue(2).Enqueue(3).Enqueue(4);
+            var assert1 = new List<int> { 2, 3, 4 };
+            circularQueue1.All((index, element) => element == assert1[index]).ToConsole("elements is {2, 3, 4}:");
+            (circularQueue1.Count == 3).ToConsole("Count is 3:");
+
+            "\n".ToConsole();
+            var circularQueue2 = new CircularQueue<int>(3).Enqueue(2).Enqueue(3).Enqueue(4);
+            (circularQueue2.Dequeue() == 2).ToConsole("element is 2:");
+            var assert2 = new List<int> { 3, 4 };
+            circularQueue2.All((index, element) => element == assert2[index]).ToConsole("elements is {3, 4}:");
+
+            "\n".ToConsole();
+            var circularQueue3 = new CircularQueue<int>(3).Enqueue(3).Enqueue(4);
+            (circularQueue3.Peek() == 3).ToConsole("element is 3:");
+            var assert3 = new List<int> { 3, 4 };
+            circularQueue3.All((index, element) => element == assert3[index]).ToConsole("elements is {3, 4}:");
+
+            "\n".ToConsole();
+            var circularQueue4 = new CircularQueue<int>(3);
+            var assert4 = new List<int> { };
+            circularQueue4.All((index, element) => element == assert4[index]).ToConsole("elements is {}:");
+
+            "\n".ToConsole();
+            try
+            {
+                var circularQueue5 = new CircularQueue<int>(3);
+                circularQueue5.Dequeue();
+            }
+            catch (Exception e)
+            {
+                (e is InvalidOperationException).ToConsole("throw InvalidOperationException:");
+            }
+        }
+
+        [DisplayMethod]
         public void DequeImplement()
         {
             var deque = new Deque<int>(Enumerable.Range(5, 2)).EnqueueHead(1).EnqueueHead(2).EnqueueTail(3).EnqueueTail(4);
             var assert  = new List<int> { 2, 1, 5, 6, 3, 4 };
-            deque.All((index, element) => element == assert[index]).ToConsole("element is { 2, 1, 5, 6, 3, 4 } :");
+            deque.All((index, element) => element == assert[index]).ToConsole("elements is { 2, 1, 5, 6, 3, 4 } :");
             
             "\n".ToConsole();
-            (deque.DequeueHead() == 2).ToConsole("DequeueHead Get Head 2");
+            var deque1 = new Deque<int>(Enumerable.Range(5, 2)).EnqueueHead(1).EnqueueHead(2).EnqueueTail(3).EnqueueTail(4);
+            (deque1.DequeueHead() == 2).ToConsole("DequeueHead Get Head 2");
             var assert1 = new List<int> { 1, 5, 6, 3, 4 };
-            deque.All((index, element) => element == assert1[index]).ToConsole("element is { 1, 5, 6, 3, 4 } :");
+            deque1.All((index, element) => element == assert1[index]).ToConsole("elements is { 1, 5, 6, 3, 4 } :");
             
             "\n".ToConsole();
-            (deque.DequeueTail() == 4).ToConsole("DequeueTail Get Head 4");
+            var deque2 = new Deque<int>(Enumerable.Range(5, 2)).EnqueueHead(1).EnqueueTail(3).EnqueueTail(4);
+            (deque2.DequeueTail() == 4).ToConsole("DequeueTail Get Head 4");
             var assert2 = new List<int> { 1, 5, 6, 3 };
-            deque.All((index, element) => element == assert2[index]).ToConsole("element is { 1, 5, 6, 3 } :");
+            deque2.All((index, element) => element == assert2[index]).ToConsole("elements is { 1, 5, 6, 3 } :");
         }
     }
 }
