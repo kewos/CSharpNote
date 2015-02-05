62e3abcb138302d7b40c80c963744423e4c78373
kewos <gn00437805@gmail.com>
2015-02-05 18:22:14 +0800

    update

--- ConsoleDisplay.Data.CSharpPracticeMethod/CSharpPracticeMethodRepository.cs
+++ ConsoleDisplay.Data.CSharpPracticeMethod/CSharpPracticeMethodRepository.cs
@@ -1531,70 +1531,7 @@ namespace ConsoleDisplay.Data.CSharpPracticeMethod
         { 
         }
 
-        public class LargeObjectHeapHandler
-        {
-            /// <summary>
-            /// Static variable used to store our 'big' block. This ensures that the block is always up for garbage collection.
-            /// </summary>
-            public byte[] bigBlock;
-
-            /// <summary>
-            /// Allocates 90,000 byte blocks, optionally intersperced with larger blocks
-            /// </summary>
-            public void Fill(bool allocateBigBlocks, bool grow, bool alwaysGC)
-            {
-                // Number of bytes in a small block
-                // 90000 bytes, just above the limit for the LOH
-                const int blockSize = 90000;
-
-                // Number of bytes in a larger block: 16Mb initially
-                int largeBlockSize = 1 << 24;
-
-                // Number of small blocks allocated
-                int count = 0;
-
-                try
-                {
-                    // We keep the 'small' blocks around
-                    // (imagine an algorithm that allocates memory in chunks)
-                    List<byte[]> smallBlocks = new List<byte[]>();
-
-                    while(true)
-                    {
-                        // Write out some status information
-                        if ((count % 1000) == 0)
-                        {
-                            Console.CursorLeft = 0;
-                            Console.Write(new string(' ', 20));
-                            Console.CursorLeft = 0;
-                            Console.Write("{0}", count);
-                            Console.CursorLeft = 0;
-                        }
-
-                        if (alwaysGC)
-                        {
-                            GC.Collect();
-                        }
-                        if (allocateBigBlocks)
-                        {
-                            bigBlock = new byte[largeBlockSize];
-                        }
-                        if (grow)
-                        {
-                            largeBlockSize++;
-                        }
-                        smallBlocks.Add(new byte[blockSize]);
-                        count++;
-                    }
-                }
-                catch (OutOfMemoryException)
-                {
-                    bigBlock = null;
-                    GC.Collect();
-                    Console.WriteLine("{1}Mb allocated", (count * blockSize) / (1024 * 1024));
-                }
-            }
-        }
+       
 
         /// <summary>
         /// 大於85000bytes的物件建立時分配於LOH(large object heap)
@@ -1614,6 +1551,42 @@ namespace ConsoleDisplay.Data.CSharpPracticeMethod
             //handler.Fill(true, true, true);
             //handler.Fill(false, true, false);
             //handler.Fill(true, false, false);
+        }
+
+        private enum PerformanceCheckEnum
+        {
+            Test
+        }
+        /// <summary>
+        /// Conclusion
+        /// Performance of EnumGetName great than Performance of Getstring
+        /// 100000times result
+        /// Performance of GetstringAction:94ms
+        /// Performance of EnumGetNameAction:46ms
+        /// </summary>
+        [DisplayMethod]
+        public void EnumToStringAndEnumGetNamePerformance()
+        {
+            var times = 100000;
+            var executeTimes = Enumerable.Range(0, times);
+            Action getNamePerformanceCheck = () =>
+            {
+                executeTimes.ForEach(n =>
+                {
+                    var t = PerformanceCheckEnum.Test.ToString();
+                });
+            };
+
+            Action toStringPerformanceCheck = () =>
+            {
+                executeTimes.ForEach(n =>
+                {
+                    var t = Enum.GetName(typeof (PerformanceCheckEnum), PerformanceCheckEnum.Test);
+                });
+            };
+
+            getNamePerformanceCheck.CaculateExcuteTime().ToConsole("GetNamePerformance:");
+            toStringPerformanceCheck.CaculateExcuteTime().ToConsole("ToStringPerformanceCheck:");
         }
     }
 }
