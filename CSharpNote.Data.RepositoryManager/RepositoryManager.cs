18d244c1cce101f16204020166247e241d8a19d8
kewos <gn00437805@gmail.com>
2015-02-13 10:23:47 +0800

    update

---
+++ CSharpNote.Data.RepositoryManager/RepositoryManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using CSharpNote.Core.Contracts;
+
+namespace CSharpNote.Data.RepositoryManager
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly IList<IMethodRepository> methodRepositories;
+
+        #region constructor
+        public RepositoryManager(IEnumerable<IMethodRepository> methodRepositories)
+        {
+            this.methodRepositories = methodRepositories.ToList();
+        }
+        #endregion
+
+        #region IRepositoryManager member
+        public IEnumerable<string> GetRepositoryNames()
+        {
+            return methodRepositories.Select(repository => repository.RepositoryName);
+        }
+
+        public IMethodRepository this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= methodRepositories.Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return methodRepositories[index];
+            }
+        }
+        #endregion
+    }
+}
