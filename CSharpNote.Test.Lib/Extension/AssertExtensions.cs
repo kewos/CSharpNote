546a4403e9dfc4b24c4aaf74c70f8b5c2368d8c2
kewos <gn00437805@gmail.com>
2015-02-16 16:14:46 +0800

    update

---
+++ CSharpNote.Test.Lib/Extension/AssertExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpNote.Test.Lib.Extension
+{
+    public static class AssertExtensions
+    {
+        public static void AssertHandleException<TException>(this Action action)
+            where TException : Exception
+        {
+            try
+            {
+                action();
+                Assert.Fail("ExceptionMustBeThrown");
+            }
+            catch (TException e)
+            {
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("IncorrectException");
+            }
+        }
+
+        public static void AssertHandleException(this Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("IncorrectException");
+            }
+        }
+    }
+}
---
+++ CSharpNote.Test.Service/Test_RepositoryManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpNote.Core.Implements;
+using CSharpNote.Service.CSharpNoteService;
+using CSharpNote.Test.Lib.Extension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpNote.Test.Service
+{
+    [TestClass]
+    public class Test_RepositoryManager
+    {
+        private class testMethodRepository : AbstractMethodRepository
+        {
+        }
+
+        private IEnumerable<testMethodRepository> GetRepositories
+        {
+            get
+            {
+                yield return new testMethodRepository();
+                yield return new testMethodRepository();
+                yield return new testMethodRepository();
+            }
+        }
+
+        private CSharperNoteService TestCSharpService
+        {
+            get
+            {
+                return new CSharperNoteService(GetRepositories);
+            }
+        }
+
+        [TestMethod]
+        public void GetRepositoryNames_Get_ReturnRepositoryNames()
+        {
+            //Arrange
+            var repositoryManager = TestCSharpService;
+
+            //Act
+            var actual = repositoryManager.GetRepositoryNames().ToList();
+
+            //Assert
+            var expect = new List<string> {"test", "test", "test"};
+            for (var i = 0; i < expect.Count; i++)
+            {
+                Assert.AreEqual(expect[i], actual[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Indexer_Get_ReturnRepository()
+        {
+            //Arrange
+            var repositoryManager = TestCSharpService;
+
+            //Act
+            var actual = Enumerable.Range(0, 3).Select(n => repositoryManager[n].RepositoryName).ToList();
+
+            //Assert
+            var expect = new List<string> { "test", "test", "test" };
+            for (var i = 0; i < expect.Count; i++)
+            {
+                Assert.AreEqual(expect[i], actual[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Indexer_CheckLowerBounded_ThrowException()
+        {
+            Action action = () =>
+            {
+                var methodInfo = TestCSharpService[-1];
+            };
+
+            action.AssertHandleException<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void Indexer_CheckUpperBounded_ThrowException()
+        {
+            Action action = () =>
+            {
+                var methodInfo = TestCSharpService[3];
+            };
+
+            action.AssertHandleException<ArgumentException>();
+        }
+    }
+}
